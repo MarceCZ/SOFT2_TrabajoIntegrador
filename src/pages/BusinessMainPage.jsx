31d5270703be71a8fde35724356e3f3f13c68770
SebaVG <sebastianvelasquezgonzales@gmail.com>
2024-10-01 12:32:43 -0500

    Prueba

--- src/pages/BusinessMainPage.jsx
+++ src/pages/BusinessMainPage.jsx
@@ -1,10 +1,26 @@
 import Header from '../components/Header';
+import ProductoCard from '../components/ProductoCard';
+import ProductoBody from '../components/ProductoBody';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import productosData from '../data/data.json';
+import { Container } from '@mui/material';
 
 const BusinessMainPage = () => {
+
+  const productoData = productosData.productos;
+
   return (
     <div>
       <Header></Header>
-      <h1>Business Main Page</h1>
+      <Container sx={{display: 'flex', flexDirection: 'column', mt: '30px', mb: '50px'}}>
+      <h1 style={{ textAlign: "center" }}>Comienza a armar tu kit</h1>
+        <ButtonGroup variant="outlined" aria-label="Basic button group" sx={{ justifyContent: "center" }}>
+          <Button>Ver productos</Button>
+          <Button>Ver tiendas</Button>
+        </ButtonGroup>
+      </Container>
+      <ProductoBody productoData={productoData} />
     </div>
   );
 }
