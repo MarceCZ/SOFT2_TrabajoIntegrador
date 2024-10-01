df56492a03ef4547f5ede7da28b957dc3a19bbe2
SebaVG <sebastianvelasquezgonzales@gmail.com>
2024-10-01 18:43:39 -0500

    Boton (Agregar producto)

--- src/pages/BusinessMainPage.jsx
+++ src/pages/BusinessMainPage.jsx
@@ -2,6 +2,7 @@ import Header from '../components/Header';
 import productosData from '../data/data.json'; // Este es el JSON que contiene los datos
 import { Container } from '@mui/material';
 import * as React from 'react';
+import Button from '@mui/material/Button';
 import TablaProductos from '../components/TablaProducto'; // Importamos el componente de la tabla
 
 const BusinessMainPage = () => {
@@ -14,6 +15,7 @@ const BusinessMainPage = () => {
       <Container sx={{ display: 'flex', flexDirection: 'column', mt: '30px', mb: '50px' }}>
         <h1 style={{ textAlign: 'center' }}>Mifarma</h1>
         <TablaProductos productos={productos} /> {/* Se pasa la variable de estado al componente TablaProductos */}
+        <Button>Agregar Productos</Button>
       </Container>
     </div>
   );
