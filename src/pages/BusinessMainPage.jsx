5399371876d027aa362abb680f5c49e6dba5af92
SebaVG <sebastianvelasquezgonzales@gmail.com>
2024-10-01 13:13:18 -0500

    Tabla (primera version)

--- src/pages/BusinessMainPage.jsx
+++ src/pages/BusinessMainPage.jsx
@@ -1,26 +1,66 @@
 import Header from '../components/Header';
-import ProductoCard from '../components/ProductoCard';
-import ProductoBody from '../components/ProductoBody';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import productosData from '../data/data.json';
 import { Container } from '@mui/material';
 
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
 const BusinessMainPage = () => {
 
-  const productoData = productosData.productos;
+  function createData(
+    name: string,
+    stock: number,
+    descripcion: string
+  ) {
+    return { name, stock, descripcion };
+  }
+
+  const rows = [
+    createData('Paracetamol', 159, 'descripcion'),
+    createData('Ibuprofeno', 237, 'descripcion'),
+    createData('Pastilla', 262, 'descripcion'),
+    createData('Cupcake', 305, 'descripcion'),
+    createData('Gingerbread', 356, 'descripcion'),
+  ];
 
   return (
     <div>
       <Header></Header>
       <Container sx={{display: 'flex', flexDirection: 'column', mt: '30px', mb: '50px'}}>
-      <h1 style={{ textAlign: "center" }}>Comienza a armar tu kit</h1>
-        <ButtonGroup variant="outlined" aria-label="Basic button group" sx={{ justifyContent: "center" }}>
-          <Button>Ver productos</Button>
-          <Button>Ver tiendas</Button>
-        </ButtonGroup>
-      </Container>
-      <ProductoBody productoData={productoData} />
+      <h1 style={{ textAlign: "center" }}>Mifarma</h1>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Producto</TableCell>
+            <TableCell align="right">Stock</TableCell>
+            <TableCell align="right">Descripción&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.stock}</TableCell>
+              <TableCell align="right">{row.descripcion}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
     </div>
   );
 }
