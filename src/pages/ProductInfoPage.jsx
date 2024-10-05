a2116f9466ef7f97821ef0679de0fe48bad34e1e
aldanaeguiluz <103043711+aldanaeguiluz@users.noreply.github.com>
2024-10-04 21:08:10 -0500

    Header extra

--- src/pages/ProductInfoPage.jsx
+++ src/pages/ProductInfoPage.jsx
@@ -4,7 +4,8 @@ import { CardMedia, Typography, Box, Button, Chip, Divider, IconButton, Containe
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import data from '../data/data.json';  // Importar el archivo JSON local
+import data from '../data/data.json';
+import Header from '../components/Header';
 
 const ProductInfoPage = () => {
     const { id } = useParams();  // Obtener el id del producto desde la URL
@@ -20,86 +21,91 @@ const ProductInfoPage = () => {
     const decrementarCantidad = () => cantidad > 1 && setCantidad(cantidad - 1);
 
     return (
-        <Box sx={{ padding: 4, maxWidth: '100%', margin: 'auto' }}>
-            <Button component={Link} to="/" sx={{ marginBottom: 2, position: 'absolute', top: 16, left: 16 }}>← Volver</Button>
+        <div>
+            <Header></Header>
+            <Box sx={{ padding: 4, maxWidth: '100%', margin: 'auto' }}>
+                
+                <Button component={Link} to="/" sx={{ marginBottom: 2, top: 16, left: 16 }}>← Volver</Button>
 
-            <Grid container spacing={4}>
-                {/* Sección izquierda: Imagen y detalles del producto */}
-                <Grid item xs={12} md={6}>
-                    <CardMedia
-                        component="img"
-                        alt={producto.nombre}
-                        height="400"  // Aumentar altura de la imagen
-                        image={producto.imagen}
-                        sx={{ objectFit: 'contain', borderRadius: '8px', width: '100%', marginBottom: 2 }}  // Ajustar ancho
-                    />
-                    <Typography variant="h4" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
-                        {producto.nombre} | {producto.marca}
-                    </Typography>
-                    <Chip label={producto.botica} color="primary" variant="outlined" sx={{ marginBottom: 2, fontSize: '16px' }} />
-                    <Typography variant="h4" color="textPrimary" sx={{ fontWeight: 'bold', marginBottom: 2, fontSize: '30px' }}>
-                        S/ {producto.precio.toFixed(2)}
-                    </Typography>
+                <Grid container spacing={4}>
+                    {/* Sección izquierda: Imagen y detalles del producto */}
+                    <Grid item xs={12} md={6}>
+                        <CardMedia
+                            component="img"
+                            alt={producto.nombre}
+                            height="400"  // Aumentar altura de la imagen
+                            image={producto.imagen}
+                            sx={{ objectFit: 'contain', borderRadius: '8px', width: '100%', marginBottom: 2 }}  // Ajustar ancho
+                        />
+                        <Typography variant="h4" sx={{ fontWeight: 'bold', marginBottom: 1 }}>
+                            {producto.nombre} | {producto.marca}
+                        </Typography>
+                        <Chip label={producto.botica} color="primary" variant="outlined" sx={{ marginBottom: 2, fontSize: '16px' }} />
+                        <Typography variant="h4" color="textPrimary" sx={{ fontWeight: 'bold', marginBottom: 2, fontSize: '30px' }}>
+                            S/ {producto.precio.toFixed(2)}
+                        </Typography>
 
-                    {/* Sección de cantidad y botón de agregar */}
-                    <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '15px' }}>
-                        <Paper elevation={3} sx={{ display: 'flex', alignItems: 'center', borderRadius: '8px', padding: '5px', marginRight: 2 }}>
-                            <IconButton color="primary" onClick={decrementarCantidad}>
-                                <RemoveIcon sx={{ fontSize: '30px' }} />
-                            </IconButton>
-                            <Typography variant="h4" sx={{ margin: '0 10px' }}>{cantidad}</Typography>
-                            <IconButton color="primary" onClick={incrementarCantidad}>
-                                <AddIcon sx={{ fontSize: '30px' }} />
-                            </IconButton>
-                        </Paper>
-                        <Box sx={{ width: '40px' }} />
-                        {/* Botón "Añadir" con tamaño de letra aumentado */}
-                        <Button variant="contained" color="success" sx={{ textTransform: 'none', fontSize: '30px', padding: '5px 20px' }}>
-                            Añadir
-                        </Button>
-                    </Container>
+                        {/* Sección de cantidad y botón de agregar */}
+                        <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '15px' }}>
+                            <Paper elevation={3} sx={{ display: 'flex', alignItems: 'center', borderRadius: '8px', padding: '5px', marginRight: 2 }}>
+                                <IconButton color="primary" onClick={decrementarCantidad}>
+                                    <RemoveIcon sx={{ fontSize: '30px' }} />
+                                </IconButton>
+                                <Typography variant="h4" sx={{ margin: '0 10px' }}>{cantidad}</Typography>
+                                <IconButton color="primary" onClick={incrementarCantidad}>
+                                    <AddIcon sx={{ fontSize: '30px' }} />
+                                </IconButton>
+                            </Paper>
+                            <Box sx={{ width: '40px' }} />
+                            {/* Botón "Añadir" con tamaño de letra aumentado */}
+                            <Button variant="contained" color="success" sx={{ textTransform: 'none', fontSize: '30px', padding: '5px 20px' }}>
+                                Añadir
+                            </Button>
+                        </Container>
+                    </Grid>
+
+                    {/* Sección derecha: Descripción, contraindicaciones y advertencias */}
+                    <Grid item xs={12} md={6}>
+                        {/* Acordeón para Descripción */}
+                        <Accordion>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="descripcion-content" id="descripcion-header">
+                                <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Descripción</Typography> {/* Aumentado */}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.descripcion}</Typography> {/* Aumentado */}
+                            </AccordionDetails>
+                        </Accordion>
+                        <Divider />
+
+                        {/* Acordeón para Contraindicaciones */}
+                        <Accordion>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="contraindicaciones-content" id="contraindicaciones-header">
+                                <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Contraindicaciones</Typography> {/* Aumentado */}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.contraindicaciones}</Typography> {/* Aumentado */}
+                            </AccordionDetails>
+                        </Accordion>
+                        <Divider />
+
+                        {/* Acordeón para Advertencias y precauciones */}
+                        <Accordion>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="advertencias-content" id="advertencias-header">
+                                <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Advertencias y precauciones</Typography> {/* Aumentado */}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.advertencias}</Typography> {/* Aumentado */}
+                            </AccordionDetails>
+                        </Accordion>
+                    </Grid>
                 </Grid>
-
-                {/* Sección derecha: Descripción, contraindicaciones y advertencias */}
-                <Grid item xs={12} md={6}>
-                    {/* Acordeón para Descripción */}
-                    <Accordion>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="descripcion-content" id="descripcion-header">
-                            <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Descripción</Typography> {/* Aumentado */}
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.descripcion}</Typography> {/* Aumentado */}
-                        </AccordionDetails>
-                    </Accordion>
-                    <Divider />
-
-                    {/* Acordeón para Contraindicaciones */}
-                    <Accordion>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="contraindicaciones-content" id="contraindicaciones-header">
-                            <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Contraindicaciones</Typography> {/* Aumentado */}
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.contraindicaciones}</Typography> {/* Aumentado */}
-                        </AccordionDetails>
-                    </Accordion>
-                    <Divider />
-
-                    {/* Acordeón para Advertencias y precauciones */}
-                    <Accordion>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="advertencias-content" id="advertencias-header">
-                            <Typography variant="h5" sx={{ fontWeight: 'bold', fontSize: '26px' }}>Advertencias y precauciones</Typography> {/* Aumentado */}
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography variant="body1" sx={{ fontSize: '20px' }}>{producto.advertencias}</Typography> {/* Aumentado */}
-                        </AccordionDetails>
-                    </Accordion>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+            </Box>
+            </div>
+            );
+        
 };
 
-export default ProductInfoPage;
+            export default ProductInfoPage;
 
 
 
