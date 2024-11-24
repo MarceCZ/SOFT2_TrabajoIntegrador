d499c56b354d4e69bb896ca5db7c42a9e9b9f1d1
Aldana <103043711+aldanaeguiluz@users.noreply.github.com>
2024-11-24 10:52:11 -0500

    pruebas finales

--- backend/src/pruebasunitarias/usuario.test.js
+++ backend/src/pruebasunitarias/usuario.test.js
@@ -1,96 +1,90 @@
-jest.mock('../services/usuario', () => ({
-    authenticateUser: jest.fn(),
-  }));
-  
-  
-  import usuarioService from '../services/usuario.js';
-  import { login } from '../controllers/usuario.js';
-  
-  
-  describe('Método login', () => {
-    let mockReq, mockRes;
-  
-  
-    beforeEach(() => {
-        mockReq = {
-            body: {},
-        };
-  
-  
-        mockRes = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        };
+import controller from '../controllers/usuario.js';
+const { login } = controller;
+
+import usuarioService from '../services/usuario.js';
+
+jest.mock('../services/usuario.js', () => ({
+  authenticateUser: jest.fn(),
+}));
+
+describe('Método login', () => {
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = {
+      body: {
+        email: 'test@mail.com',
+        password: '12345678',
+      },
+    };
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Debe devolver 200 y el usuario si el correo y contraseña son correctos', async () => {
+    usuarioService.authenticateUser.mockResolvedValue({
+      id: 1,
+      email: 'test@mail.com',
     });
-  
-  
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-  
-  
-    test('Debe devolver 200 y el usuario si el correo y contraseña son correctos', async () => {
-        const mockUser = { id: 1, email: 'test@mail.com'};
-        usuarioService.authenticateUser.mockResolvedValue(mockUser);
-  
-  
-        mockReq.body = { email: 'test@mail.com', password: '12345678' };
-  
-  
-        await login(mockReq, mockRes);
-  
-  
-        expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', '12345678');
-        expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith(mockUser);
-    });
-  
-  
-    test('Debe devolver 500 si el usuario no existe', async () => {
-        usuarioService.authenticateUser.mockResolvedValue(null);
-  
-  
-        mockReq.body = { email: 'noexiste@mail.com', password: '12345678' };
-  
-  
-        await login(mockReq, mockRes);
-  
-  
-        expect(usuarioService.authenticateUser).toHaveBeenCalledWith('noexiste@mail.com', '12345678');
-        expect(mockRes.status).toHaveBeenCalledWith(500);
-        expect(mockRes.json).toHaveBeenCalledWith({ message: 'Usuario o contraseña incorrectos.' });
-    });
-  
-  
-    test('Debe devolver 500 si la contraseña es incorrecta', async () => {
-        usuarioService.authenticateUser.mockResolvedValue(null);
-  
-  
-        mockReq.body = { email: 'test@mail.com', password: 'contraincorrecta' };
-  
-  
-        await login(mockReq, mockRes);
-  
-  
-        expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', 'contraincorrecta');
-        expect(mockRes.status).toHaveBeenCalledWith(500);
-        expect(mockRes.json).toHaveBeenCalledWith({ message: 'Usuario o contraseña incorrectos.' });
-    });
-  
-  
-    test('Debe devolver 500 si ocurre un error en el servidor', async () => {
-        usuarioService.authenticateUser.mockRejectedValue(new Error('Error al conectarse al servidor'));
-  
-  
-        mockReq.body = { email: 'test@mail.com', password: '12345678' };
-  
-  
-        await login(mockReq, mockRes);
-  
-  
-        expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', '12345678');
-        expect(mockRes.status).toHaveBeenCalledWith(500);
-        expect(mockRes.json).toHaveBeenCalledWith({ message: 'Error en el servidor.' });
+
+    await login(req, res);
+
+    expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', '12345678');
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      id: 1,
+      email: 'test@mail.com',
     });
   });
+
+  it('Debe devolver 500 si el usuario no existe', async () => {
+    usuarioService.authenticateUser.mockResolvedValue(null);
+
+    await login(req, res);
+
+    expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', '12345678');
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Usuario o contraseña incorrectos.',
+    });
+  });
+
+  it('Debe devolver 500 si la contraseña es incorrecta', async () => {
+    usuarioService.authenticateUser.mockResolvedValue(null);
+
+    req.body.password = 'contraincorrecta';
+
+    await login(req, res);
+
+    expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', 'contraincorrecta');
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Usuario o contraseña incorrectos.',
+    });
+  });
+
+  it('Debe devolver 500 si ocurre un error en el servidor', async () => {
+    usuarioService.authenticateUser.mockRejectedValue(new Error('Error en el servidor'));
+
+    await login(req, res);
+    expect(usuarioService.authenticateUser).toHaveBeenCalledWith('test@mail.com', '12345678');
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Error en el servidor.',
+    });
+  });
+});
+
   
